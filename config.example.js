24dce8ba09942b234dc887f0c9d28b838f5ee3d4
payrollbfp <nurlaili@bintangfajarpersada.com>
2026-09-09 01:37:32 +0700

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_ANON_KEY"
+  SUPABASE_URL: "https://dvhjaxqpoqpeyamtlvgh.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2aGpheHFwb3FwZXlhbXRsdmdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMzU1MjAsImV4cCI6MjA5MzcxMTUyMH0.qwX2LZ-9A1ZoLaqkRnZcDValUNKCYoqMvA_Vs79NkWY"
 };
