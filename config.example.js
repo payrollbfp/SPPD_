ba2713d42a82f7099e5b09686e0a1eac9e0409db
payrollbfp <nurlaili@bintangfajarpersada.com>
2026-09-09 01:35:41 +0700

    Delete config.example.js

--- config.example.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://dvhjaxqpoqpeyamtlvgh.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2aGpheHFwb3FwZXlhbXRsdmdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMzU1MjAsImV4cCI6MjA5MzcxMTUyMH0.qwX2LZ-9A1ZoLaqkRnZcDValUNKCYoqMvA_Vs79NkWY"
-};
